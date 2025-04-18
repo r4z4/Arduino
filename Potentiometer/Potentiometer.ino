c0f8865a86a6edb99150499e80604187080251c2
r4z4 <r4z4aa@gmail.com>
2025-04-17 19:30:23 -0500

    LED brightness w/ potentiometer

--- Potentiometer/Potentiometer.ino
+++ Potentiometer/Potentiometer.ino
@@ -7,7 +7,9 @@ void setup()
 void loop() 
 {
   int sensorValue = analogRead(A0);  // Read input on Analog A0
-  float voltage = semsprValue * (5.0 / 1023.0);
+  float voltage = sensorValue * (5.0 / 1023.0);
+  int brightness = sensorValue/4; // 1023/4 = 255.75 (.75 goes away since its an int)
+  analogWrite(11, brightness); // Send brightness value to LED pin, which we set on 11
   Serial.print("ADC Reading: ");
   Serial.print(sensorValue);
   Serial.print(" Analog voltage: ")
