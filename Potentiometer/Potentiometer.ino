2beffdd545904dbea891f57630de8903140f46a6
r4z4 <r4z4aa@gmail.com>
2025-04-17 19:21:06 -0500

    adc conversion to voltage

--- Potentiometer/Potentiometer.ino
+++ Potentiometer/Potentiometer.ino
@@ -7,6 +7,10 @@ void setup()
 void loop() 
 {
   int sensorValue = analogRead(A0);  // Read input on Analog A0
-  Serial.println(sensorValue);
+  float voltage = semsprValue * (5.0 / 1023.0);
+  Serial.print("ADC Reading: ");
+  Serial.print(sensorValue);
+  Serial.print(" Analog voltage: ")
+  Serial.println(voltage);
   delay(1);
 }
